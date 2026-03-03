0ccc2cd8a439a1fcf70cfa255f11994c48f9dcf0
vendaitharshada-code <vendaitharshada@gmail.com>
2026-03-03 17:23:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Student App</h1>
+      <Demo1></Demo1>
+    </div>
+  );
+}
+
+export default App;
